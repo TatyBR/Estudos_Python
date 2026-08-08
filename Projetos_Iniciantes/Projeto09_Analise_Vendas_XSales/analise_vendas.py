c24c687d2b38764fb2c4dbf626bfb862d0e34ed2
Taíta <taytaramos@hotmail.com>
2026-08-08 18:56:14 -0300

    Conhecendo a biblioteca openpyxl

--- Projetos_Iniciantes/Projeto09_Analise_Vendas_XSales/analise_vendas.py
+++ Projetos_Iniciantes/Projeto09_Analise_Vendas_XSales/analise_vendas.py
@@ -1,4 +1,91 @@
 #%%
 import pandas as pd
+import numpy as np
 
 #%%
+# excel = openpyxl 
+df_xsales = pd.read_excel("dados/BaseDados.xlsx")
+
+# %%
+df_xsales.head()
+
+#%%
+df_xsales.tail()
+# %%
+
+df_xsales.info()
+# %%
+
+print(f"O DataFrame possui {df_xsales.shape[0]} linhas e {df_xsales.shape[1]} colunas.")
+
+# %%
+
+df_xsales[['Valor Total', 'Desconto', 'Valor Total c/ Desconto','Custo Total']].describe().round(2)
+
+# %%,
+# Alterando o nome das colunas:
+colunas_novas = {
+    'Tipos de Clientes': 'tipo_clientes',
+    'País': 'pais',
+    'Produto': 'produto',
+    'Valor Total': 'valor_total',
+    'Desconto': 'desconto',
+    'Valor Total c/ Desconto': 'desconto_total',
+    'Custo Total':'custo_total',
+    'Data':'data'
+}
+df_xsales.rename(columns=colunas_novas, inplace=True)
+
+#%%
+df_xsales.head()
+
+# %%
+
+# A empresa gerou Lucro ou Prejuízo no período?
+# - Qual período? Como não especificou: todo período.
+# - Base: set/2018 até dez/2019, 2018 incompleto!!!
+# - Calcular o lucro: Valor com desconto - Custo total
+# - Verificar se é $ ou R$, antes de especificar.
+
+df_xsales['lucro'] = df_xsales['desconto_total'] - df_xsales['custo_total']
+
+# %%
+df_xsales.head()
+
+# %%
+
+# Há valores de lucro negativo (Prejuízo)?
+df_xsales[df_xsales['lucro']<0]
+
+# %%
+
+resultado_periodo = df_xsales['lucro'].sum()
+# :, representa a separação do milhar
+print(f"O resultado do período foi de $ {resultado_periodo.round(2):,}")
+
+# %%
+
+# as colunas agrupadas viram índice, no caso, a coluna data virou um índice
+df_xsales_agrup_data = df_xsales.groupby('data').agg({
+        'desconto_total': np.sum
+})
+
+# %%
+
+# Resetando o índice (coluna data)
+df_xsales_agrup_data.reset_index(inplace=True)
+
+# %%
+
+df_xsales_agrup_data.head()
+
+# %%
+df_xsales_agrup_data.columns
+# %%
+
+# Qual o faturamento Médio Mensal desta Empresa?
+
+media_periodo = df_xsales_agrup_data['desconto_total'].mean()
+print(f"O resultado do período foi de $ {media_periodo.round(2):,}")
+
+# %%
---
+++ Python_Automacao/openpyxl_comandos_basicos.py
@@ -0,0 +1,30 @@
+from openpyxl import Workbook
+
+# Criado um arquivo excel
+wb = Workbook()
+
+# Verificando as abas existentes
+print(wb.sheetnames)
+
+# Cria uma nova aba
+# wb.create_sheet("Minha_Planilha")
+
+# Selecionando a aba
+sheet = wb["Sheet"]
+
+# inserindo dados
+sheet["A1"].value = "Python"
+sheet["B1"].value = "Formação Expert"
+sheet["C1"].value = "Automação de Processos"
+
+# inserindo utilizando as coordenadas
+# Lembrando que a coluna A seria 1, B 2, C 3.... Inicia em 1...
+sheet.cell(row=2, column=1).value = "Daxus"
+sheet.cell(2, 2).value = "Automação no Python"
+
+# Apagando dados (podem ser por rótulo ou coordenadas)
+sheet["A2"].value = None
+sheet.cell(2, 2).value = None
+
+wb.save('comandos_basicos.xlsx')
+
